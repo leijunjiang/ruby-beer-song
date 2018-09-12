a7e356bddc82beaa3473e3e08e2acbbaa280ecfa
leijun-jiang <leijunjiangjob@gmail.com>
2018-09-12 20:06:18 +0200

    seconde

---
+++ beer_song.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+puts "hello"
+=begin
+def onesong(n)
+	puts "#{n} bottles of beer on the wall, #{n} bottles of beer.
+	Take one down and pass it around, #{n-1} bottles of beer on the wall."}
+end
+
+i = 99
+while i > 0 do
+   onesong(i)
+   i = i - 1
+end
+=end
