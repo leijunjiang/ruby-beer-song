cdb7ec754dc6cbcc63af5bd8a013a49cbdc92e80
leijun-jiang <leijunjiangjob@gmail.com>
2018-09-12 20:42:28 +0200

    5 assertions

--- beer_song.rb
+++ beer_song.rb
@@ -1,14 +1,46 @@
 #!/usr/bin/env ruby
-puts "hello"
+class BeerSong
+
+	def initialize
+	end
 =begin
-def onesong(n)
-	puts "#{n} bottles of beer on the wall, #{n} bottles of beer.
-	Take one down and pass it around, #{n-1} bottles of beer on the wall."}
+	@@var1 = 99
+	@@var2 = 98
+
+
+	#def onesong(n)
+	#	puts "#{n} bottles of beer on the wall, #{n} bottles of beer.
+	#	Take one down and pass it around, #{n-1} bottles of beer on the wall."
+	#end
+  @@i = 99
+
+  		while @@i > 0 do
+		   puts "#{@@var1} bottles of beer on the wall, #{@@var1} bottles of beer.
+			Take one down and pass it around, #{@@var2} bottles of beer on the wall."
+		   @@i = @@i - 1
+		   @@var1 -= 1
+			 @@var2 -= 1
+				end	
+
+=end
+
+  def verse(nombre)
+  	if nombre >= 3
+			"#{nombre} bottles of beer on the wall, #{nombre} bottles of beer.\n" \
+    	"Take one down and pass it around, #{nombre - 1} bottles of beer on the wall.\n"
+		elsif nombre == 2
+			"#{nombre} bottles of beer on the wall, #{nombre} bottles of beer.\n" \
+    	"Take one down and pass it around, #{nombre - 1} bottle of beer on the wall.\n"
+  	elsif nombre == 1
+			"#{nombre} bottle of beer on the wall, #{nombre} bottle of beer.\n" \
+    	"Take it down and pass it around, no more bottles of beer on the wall.\n"
+  	elsif nombre == 0
+			"No more bottles of beer on the wall, no more bottles of beer.\n" \
+      "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+  	end
+
+	end
 end
 
-i = 99
-while i > 0 do
-   onesong(i)
-   i = i - 1
-end
-=end
+
+
